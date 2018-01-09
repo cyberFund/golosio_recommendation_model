31940b20c4ebfef29ebc37f8509e444d7993487c
Blackmoon <belchikov1996@gmail.com>
2018-01-09 15:47:52 +0300

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-(crontab -l ; echo "0 5 * * * python `pwd`/model/lda.py") | crontab  -
-(crontab -l ; echo "0 5 * * * python `pwd`/model/ann.py") | crontab  -
-(crontab -l ; echo "0 5 * * * python `pwd`/model/train.py") | crontab  -
-(crontab -l ; echo "0 5 * * * python `pwd`/model/predict.py") | crontab  -
+(crontab -l ; echo "0 */5 * * * python3 `pwd`/model/lda.py $2 $3") | crontab  -
+(crontab -l ; echo "0 */5 * * * python3 `pwd`/model/ann.py $2 $3") | crontab  -
+(crontab -l ; echo "0 */5 * * * python3 `pwd`/model/train.py $1 $2 $3") | crontab  -
+(crontab -l ; echo "0 */5 * * * python3 `pwd`/model/predict.py $1 $2 $3") | crontab  -
