1149875ea4df58757fe7d1771091d6e33f0aa566
Serge Nedashkovsky <snedashkovsky@gmail.com>
2018-02-01 18:00:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ mv * ../
 cd ../
 rm -rf ./golosio-recommendation-model
 
-(crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/lda.py $1 $2") | crontab  -
 (crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/doc2vec.py $1 $2") | crontab  -
-(crontab -l ; echo "0 */5 * * * cd `pwd` && python3 ./model/ann.py $1 $2") | crontab  -
+(crontab -l ; echo "30 */5 * * * cd `pwd` && python3 ./model/ann.py $1 $2") | crontab  -
 (crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/train.py $1 $2") | crontab  -
-(crontab -l ; echo "0 */3 * * * cd `pwd` && python3 ./model/predict.py $1 $2") | crontab  -
+(crontab -l ; echo "30 */5 * * * cd `pwd` && python3 ./model/predict.py $1 $2") | crontab  -
