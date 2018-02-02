f7c40c78f3ef436692d0fd8c934664d02d8da471
Anatoli <belchikov1996@mail.ru>
2018-02-02 17:04:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-./gdrive download --recursive 1h1xEERnG7Z69KGSXLIjZj5e3ygh5oeL4
-cd ./golosio-recommendation-model
-mv * ../
-cd ../
-rm -rf ./golosio-recommendation-model
-
 (crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/doc2vec.py $1 $2") | crontab  -
 (crontab -l ; echo "30 */5 * * * cd `pwd` && python3 ./model/ann.py $1 $2") | crontab  -
 (crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/train.py $1 $2") | crontab  -
