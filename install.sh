fb0ab1f632e1d6b9c98426acf0175c61cd4f940d
Blackmoon <belchikov1996@gmail.com>
2018-02-05 19:26:24 +0300

    Modified install file

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ cd ../
 rm -rf ./golosio-recommendation-model
 
 (crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/doc2vec.py $1 $2") | crontab  -
-(crontab -l ; echo "30 */5 * * * cd `pwd` && python3 ./model/ann.py $1 $2") | crontab  -
-(crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/train.py $1 $2") | crontab  -
-(crontab -l ; echo "30 */5 * * * cd `pwd` && python3 ./model/predict.py $1 $2") | crontab  -
+(crontab -l ; echo "5 */10 * * * cd `pwd` && python3 ./model/ann.py $1 $2") | crontab  -
+(crontab -l ; echo "10 */10 * * * cd `pwd` && python3 ./model/train.py $1 $2") | crontab  -
+(crontab -l ; echo "15 */10 * * * cd `pwd` && python3 ./model/predict.py $1 $2") | crontab  -
