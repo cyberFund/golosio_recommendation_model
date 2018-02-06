40f1cdb39d641751f55bb261914f86b47ffc88cb
Blackmoon <belchikov1996@gmail.com>
2018-02-06 13:26:57 +0300

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-(crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/train/doc2vec.py $1 $2") | crontab  -
-(crontab -l ; echo "0 */5 * * * cd `pwd` && python3 ./model/train/ann.py $1 $2") | crontab  -
-(crontab -l ; echo "0 */10 * * * cd `pwd` && python3 ./model/train/ffm.py $1 $2") | crontab  -
+(crontab -l ; echo "0 */10 * * * doc2vec_train $1 $2") | crontab  -
+(crontab -l ; echo "0 */5 * * * ann_train $1 $2") | crontab  -
+(crontab -l ; echo "0 */10 * * * ffm_train $1 $2") | crontab  -
 
-(crontab -l ; echo "0 */1 * * * cd `pwd` && python3 ./model/predict/doc2vec.py $1 $2") | crontab  -
-(crontab -l ; echo "0 */1 * * * cd `pwd` && python3 ./model/predict/ann.py $1 $2") | crontab  -
-(crontab -l ; echo "0 */5 * * * cd `pwd` && python3 ./model/predict/ffm.py $1 $2") | crontab  -
+(crontab -l ; echo "0 */1 * * * doc2vec_predict $1 $2") | crontab  -
+(crontab -l ; echo "0 */1 * * * ann_predict $1 $2") | crontab  -
+(crontab -l ; echo "0 */5 * * * ffm_predict $1 $2") | crontab  -
