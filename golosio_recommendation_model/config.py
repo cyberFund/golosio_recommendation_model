ef97a1a8263918c2b5a312c2b1b2267d38c47a74
Anatoli <belchikov1996@mail.ru>
2018-03-01 20:33:53 +0300

    Update config.py

--- golosio_recommendation_model/config.py
+++ golosio_recommendation_model/config.py
@@ -2,7 +2,7 @@ config = {
   'database_url': "localhost:27017", # Your mongo database url
   'database_name': "golos_comments", # Mongo database with dumps content
   'events_path': "/var/lib/mysql-files/test_events7.csv", # Path to csv file with events
-  'accounts_path': "/home/anatoli/Documents/golosio_recommendation_model/accounts.csv", # Path to csv file with accounts
+  'accounts_path': "/home/anatoli/Documents/golosio_recommendation_model/accounts.csv", # Path to csv file with accounts, only for debug
   'node_url': 'ws://localhost:8090', # Golos.io websocket url
   'model_path': "/tmp/", # Path to model files
   'log_path': "/tmp/recommendation_model.log", # Path to model log
