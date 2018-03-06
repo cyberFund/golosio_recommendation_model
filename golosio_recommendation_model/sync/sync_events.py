4849d46cf91c179b46a7111bc73088643da4878c
Anatoli <belchikov1996@mail.ru>
2018-03-06 16:53:51 +0300

    Update sync_events.py

--- golosio_recommendation_model/sync/sync_events.py
+++ golosio_recommendation_model/sync/sync_events.py
@@ -3,15 +3,45 @@ from pymongo import MongoClient
 import sys
 from golosio_recommendation_model.model import utils
 from golosio_recommendation_model.config import config
+import pymysql
 
 @utils.error_log("Sync events")
+def get_events(events_host, events_database, events_user, events_password):
+  result = []
+  connection = pymysql.connect(host=events_host,
+                             user=events_user,
+                             password=events_password,
+                             db=events_database,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+  try:
+    with connection.cursor() as cursor:
+      sql = """
+SELECT user_id, event_type, value, refurl, created_at
+FROM golos.web_events 
+WHERE 
+   (event_type = "Comment" OR event_type = "Vote" OR event_type = "PageView") 
+   AND created_at < CURDATE()
+   AND created_at >  CURDATE() - INTERVAL 1 DAY
+"""
+      cursor.execute(sql)
+      result = cursor.fetchall()
+  finally:
+    connection.close()
+  return result
+    
 def sync_events():
   url = config['database_url']
   database = config['database_name']
   events_path = config['events_path']
+  events_database = config['events_database']
   utils.log("Sync events", "Get events from a file...")
-  events = pd.read_csv(events_path, names=['user_id', 'event_type', 'value', 'refurl', 'created_at'])
-  events["created_at"] = pd.to_datetime(events["created_at"])
+  events = get_events(
+    events_database['host'], 
+    events_database['database'], 
+    events_database['user'], 
+    events_database['password']
+  )
   client = MongoClient(url)
   db = client[database]
-  db.raw_event.insert_many(events.to_dict('records'))
+  db.raw_event.insert_many(events)
