731a23f5676de58ba4a266152c84bbf395397ba5
Anatoli <belchikov1996@mail.ru>
2018-03-13 20:06:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
+python3 -m nltk.downloader all
+
 (crontab -l ; echo "0 0 * * * doc2vec_train start") | crontab  -
 (crontab -l ; echo "0 5 * * * ann_train start") | crontab  -
 (crontab -l ; echo "0 10 * * * ffm_train start") | crontab  -
 
 sync_comments start
 recommendations_server start
+
